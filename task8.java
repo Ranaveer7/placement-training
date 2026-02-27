27f8cc17187603f4ad97ffcd4c1f282c61b4792c
Ranaveer7 <vtu24807@veltech.edu.in>
2026-02-27 10:29:33 +0530

    Add files via upload

---
+++ task8.java
@@ -0,0 +1,31 @@
+class task8 {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> result = new ArrayList<>();
+
+        for (String word : words) {
+            if (match(word, pattern)) {
+                result.add(word);
+            }
+        }
+        return result;
+    }
+
+    private boolean match(String word, String pattern) {
+        if (word.length() != pattern.length()) return false;
+
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+
+            if (!map1.containsKey(w)) map1.put(w, p);
+            if (!map2.containsKey(p)) map2.put(p, w);
+
+            if (map1.get(w) != p || map2.get(p) != w)
+                return false;
+        }
+        return true;
+    }
+}
